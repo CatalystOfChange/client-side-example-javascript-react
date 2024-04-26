c8b5491587a481ff3061d4372efeeb229e61be6b
Web Alchemy <CatalystOfChange@pm.me>
2024-04-26 11:03:54 -0500

    delete secret key

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,6 @@
 export const envConfig = {
   vapi: {
     apiUrl: import.meta.env.VITE_VAPI_API_URL ?? "https://api.vapi.ai",
-    token: import.meta.env.VITE_VAPI_WEB_TOKEN ?? "9780018f-4650-4ce8-ac82-12d163ee16d2",
+    token: import.meta.env.VITE_VAPI_WEB_TOKEN ?? "",
   },
 };
